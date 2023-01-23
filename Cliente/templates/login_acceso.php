4b12dba19ea071a250ee66e4dc11b5ce72cef218
dannyaguilargil <danny.aguilar.gil@gmail.com>
2023-01-22 22:34:47 -0500

    agregando scss

--- Cliente/templates/login_acceso.php
+++ Cliente/templates/login_acceso.php
@@ -7,7 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
-    <link rel="stylesheet" href="../css/estilos_login_acceso.css">
+     <!--<link rel="stylesheet" href="../css/estilos_login_acceso.css"> PRACTICA SCSS-->
+    <link rel="stylesheet" href="../scss/login_acceso.scss">
     <link rel="icon" href="imgs/logoimsaludrecortado.ico">
     <title>Iniciar Sesion</title>
 </head>
